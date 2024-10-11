4b38e41defa2561596c537eb410af95150c048a0
Siva Narisetty <sivanarisetty@geotab.com>
2024-10-11 17:10:19 -0400

    Add implementation for AddCountry method and make sure tests are passing

--- ContactsManager.Application/Services/CountryService.cs
+++ ContactsManager.Application/Services/CountryService.cs
@@ -3,11 +3,33 @@ using ContactsManager.Core.Interfaces;
 
 namespace ContactsManager.Application.Services;
 
-//inheritdoc
+/// <inheritdoc />
 public class CountryService : ICountryService
 {
+    private readonly IList<Country> _countries = new List<Country>();
+
     public Country AddCountry(Country country)
     {
-        throw new NotImplementedException();
+        if (country is null)
+        {
+            throw new ArgumentNullException(nameof(country));
+        }
+
+        if (string.IsNullOrEmpty(country.CountryName))
+        {
+            throw new ArgumentException("Country name is required", nameof(country.CountryName));
+        }
+
+        if (_countries.Any(c => c.CountryName == country.CountryName))
+        {
+            throw new ArgumentException("Country name must be unique", nameof(country.CountryName));
+        }
+        
+        // This has to happen on the database side automatically
+        // we are doing this here only because we are using a dummy database
+        country.CountryId = Guid.NewGuid();
+        _countries.Add(country);
+
+        return country;
     }
 }
--- ContactsManager.Tests/CountryServiceTests.cs
+++ ContactsManager.Tests/CountryServiceTests.cs
@@ -1,14 +1,17 @@
+using ContactsManager.Application.Services;
 using ContactsManager.Core.Entities;
 using ContactsManager.Core.Interfaces;
 
 namespace ContactsManager.Tests;
 
-public class CountryServiceTests(ICountryService countryService)
+public class CountryServiceTests
 {
+    private readonly ICountryService _countryService = new CountryService();
+
     [Fact]
     public void AddCountry_NullInput_ThrowsArgumentNullException()
     {
-        Assert.Throws<ArgumentNullException>(() => countryService.AddCountry(null!));
+        Assert.Throws<ArgumentNullException>(() => _countryService.AddCountry(null!));
     }
 
     [Fact]
@@ -16,17 +19,17 @@ public class CountryServiceTests(ICountryService countryService)
     {
         Country input = new () {CountryName = string.Empty};
         
-        Assert.Throws<ArgumentException>(() => countryService.AddCountry(input));
+        Assert.Throws<ArgumentException>(() => _countryService.AddCountry(input));
     }
     
     [Fact]
     public void AddCountry_CountryNameDuplicate_ThrowsArgumentException()
     {
         Country existingCountry = new() { CountryName = "India" };
-        countryService.AddCountry(existingCountry);
+        _countryService.AddCountry(existingCountry);
         Country newCountry = new() { CountryName = "India" };
         
-        Assert.Throws<ArgumentException>(() => countryService.AddCountry(newCountry));
+        Assert.Throws<ArgumentException>(() => _countryService.AddCountry(newCountry));
     }
 
     [Fact]
@@ -34,7 +37,7 @@ public class CountryServiceTests(ICountryService countryService)
     {
         Country input = new () {CountryName = "India"};
         
-        var actual = countryService.AddCountry(input);
+        var actual = _countryService.AddCountry(input);
         
         Assert.NotEqual(Guid.Empty, actual.CountryId);
         Assert.Equal(input.CountryName, actual.CountryName);
