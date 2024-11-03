7c82d8d0929b870ab7c93632b8b543766094768d
Siva Narisetty <sivanarisetty@geotab.com>
2024-11-03 09:30:11 -0500

    Add migration for insert person stored procedure

---
+++ ContactsManager.Core/Migrations/20241102192919_InsertPersonStoredProcedure.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactsManager.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertPersonStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string createInserPersonSp = @"
+                 CREATE OR REPLACE PROCEDURE insert_person(
+                    p_person_id UUID,
+                    p_person_name VARCHAR(50),
+                    p_date_of_birth TIMESTAMPTZ,
+                    p_gender INT,
+                    p_email_address VARCHAR(50),
+                    p_country_id UUID
+                )
+                LANGUAGE plpgsql
+                AS $$
+                BEGIN
+                    INSERT INTO persons (person_id, person_name, date_of_birth, gender, email_address, country_id)
+                    VALUES (p_person_id, p_person_name, p_date_of_birth, p_gender, p_email_address, p_country_id);
+                END;
+                $$;           
+            ";
+            migrationBuilder.Sql(createInserPersonSp);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP PROCEDURE IF EXISTS insert_person(
+                    UUID,
+                    VARCHAR(50),
+                    TIMESTAMPTZ,
+                    INT,
+                    VARCHAR(50),
+                    UUID
+                );
+            ");
+        }
+    }
+}
